8f843e957f29ff2c2fc2a2887bc14aac0b551b0d
Leonardo Melendez <leonardo.melendez@uabc.edu.mx>
2020-07-12 15:26:27 -0700

    Windows Powershell setup script

---
+++ script/winsetup.ps1
@@ -0,0 +1,53 @@
+# Disable Advertising ID 
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo -Name Enabled -Type DWord -Value 0
+
+# Disable Activity Tracking
+@('EnableActivityFeed','PublishUserActivities','UploadUserActivities') |% { Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\System -Name $_ -Type DWord -Value 0 }
+
+# Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+
+# Opens PC to This PC, not quick access
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+
+# Get Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Fonts
+choco install dejavufonts -y
+choco install font-nerd-DejaVuSansMono -y
+
+# Media
+choco install honeyview -y
+choco install mpv -y
+choco install paint.net -y
+choco install aimp -y
+
+# Security
+choco install malwarebytes -y
+choco install keepassxc -y
+
+# Dev
+choco install git -y
+choco install python -y
+choco install notepadplusplus -y
+choco install vscodium -y
+choco install microsoft-windows-terminal -y
+
+# Internet
+choco install chromium -y
+choco install qbittorrent -y
+
+# Games
+choco install steam -y
+
+# Additional Tools
+choco install 7zip -y
+choco install sumatrapdf -y
+choco install curl -y
+choco install wget -y
+choco install windirstat -y
+choco install rufus -y
+choco install bleachbit -y
+choco install Everything -y
+choco install f.lux -y
